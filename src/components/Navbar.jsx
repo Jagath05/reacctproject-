6fa300967de534c07620124ef796690d2693699f
jagath05 <jagathrachagan285@gmail.com>
2026-02-16 22:45:55 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Camera, ChevronDown,Search, UserRound } from "lucide-react";
+
 export default function Navbar() {
   return (
-    <div className="shadow-md bg-white">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-primary">Freshly</h1>
+    <div className="flex items-center gap-16 px-8 py-7">
 
-        <div className="flex gap-4 items-center">
-          <input
-            type="text"
-            placeholder="Search products..."
-            className="border px-4 py-2 rounded-lg w-64"
-          />
-          <button className="bg-primary text-white px-4 py-2 rounded-lg">
-            Search
-          </button>
+      {/* Logo */}
+      <h1 className="text-5xl font-semibold text-purple-600">
+        Freshly
+      </h1>
+
+      {/* Category Pill Button */}
+      <div className="flex items-center gap-3 border border-gray-300 rounded-full px-6 py-5 mt-3 h-14 cursor-pointer hover:shadow-md transition">
+
+        {/* Green 3-line icon */}
+        <div className="flex flex-col gap-1">
+          <span className="w-6 h-[2px] bg-green-600"></span>
+          <span className="w-5 h-[2px] bg-green-600"></span>
+          <span className="w-4 h-[2px] bg-green-600"></span>
         </div>
 
-        <div className="flex gap-4">
-          <button>🛒</button>
-          <button>👤</button>
-        </div>
+        {/* Text */}
+        <span className="text-gray-700 font-medium text-lg">
+          All Categories
+        </span>
+        
+
+        {/* Dropdown Arrow */}
+        <ChevronDown size={20} className="text-green-600" />
+
       </div>
-    </div>
+       {/* Search Bar */}
+      <div className="flex items-center bg-gray-100 border border-gray-300 rounded-full overflow-hidden mt-3 w-155 h-14">
+
+        {/* Input */}
+        <input
+          type="text"
+          placeholder="Type Your Products ..."
+          className="flex-1 px-6 h-full  outline-none text-gray-600"
+        />
+
+        {/* Search Button */}
+        <button className="bg-yellow-400 border-2 rounded-r-4xl border-gray-200 px-8 h-full flex items-center gap-2 font-medium hover:bg-yellow-500 transition ">
+          Search
+          <Search size={18} />
+        </button>
+
+      </div>
+<UserRound size={72} strokeWidth={1} absoluteStrokeWidth />    
+</div>
   );
 }
-
-
-
-
-// import React from 'react'
-
-// export default function Navbar() {
-//   return (
-//     <div className="bg-sky-400 w-full h-20 ">
-// <h1 className="text-red-500 text-5xl py-4"> welcome to our website</h1>
-//       </div>
-//   )
-// }
